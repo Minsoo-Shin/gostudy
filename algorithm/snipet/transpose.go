e6443e7f3c0bb3cc4303edc3e7299b704691abe6
brandon-shin <alstn5038@gmail.com>
2023-12-31 02:30:28 +0900

    [go/algorithm] transpose

---
+++ algorithm/snipet/transpose.go
@@ -0,0 +1,19 @@
+package snipet
+
+func Transpose(matrix [][]int) [][]int {
+	rows := len(matrix)
+	cols := len(matrix[0])
+
+	result := make([][]int, cols)
+	for i := range result {
+		result[i] = make([]int, rows)
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			result[j][i] = matrix[i][j]
+		}
+	}
+
+	return result
+}
---
+++ algorithm/snipet/transpose_test.go
@@ -0,0 +1,58 @@
+package snipet
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTranspose(t *testing.T) {
+	tests := []struct {
+		name      string
+		twoDArray [][]int
+		want      [][]int
+	}{
+		{
+			name: "square",
+			twoDArray: [][]int{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9},
+			},
+			want: [][]int{
+				{1, 4, 7},
+				{2, 5, 8},
+				{3, 6, 9},
+			},
+		},
+		{
+			name: "1x3 => 3x1",
+			twoDArray: [][]int{
+				{1, 2, 3},
+			},
+			want: [][]int{
+				{1},
+				{2},
+				{3},
+			},
+		},
+		{
+			name: "3x2 => 2x3",
+			twoDArray: [][]int{
+				{1, 2},
+				{3, 4},
+				{5, 6},
+			},
+			want: [][]int{
+				{1, 3, 5},
+				{2, 4, 6},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Transpose(tt.twoDArray); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Transpose() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
